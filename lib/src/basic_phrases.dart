0ae1a83c5dc45dc211ae01a8fd093be886d7a20d
Timi Petre <gaxxis@yahoo.com>
2021-11-18 22:57:27 +0200

    fixed type annotation

--- lib/src/basic_phrases.dart
+++ lib/src/basic_phrases.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class BasicPhrases extends StatefulWidget {
   const BasicPhrases({
@@ -12,21 +13,15 @@ class BasicPhrases extends StatefulWidget {
 }
 
 class _BasicPhrasesState extends State<BasicPhrases> {
-  final List<String> data = <String>[
-    'da',
-    'nu',
-    'fix',
-    'da',
-    'nu',
-    'fix',
-    'da',
-    'nu',
-    'fix',
-    'da',
-    'nu',
-    'fix',
-  ];
-  int index = 0;
+  Future<void> playerS() async {
+    final AudioPlayer player = AudioPlayer();
+    // final List<Future<Duration?>> duration = <Future<Duration?>>[
+    //   player.setUrl('https://www.pacdv.com/sounds/voices/okay-7.wav'),
+    // ];
+    player.play();
+  }
+
+  final List<String> data = <String>['okay', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
 
   @override
   Widget build(BuildContext context) {
@@ -45,13 +40,26 @@ class _BasicPhrasesState extends State<BasicPhrases> {
             mainAxisSpacing: 16,
           ),
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(15),
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  playerS();
+                });
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(
+                  data[index],
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-              child: Text(data[index]),
             );
           }),
     );
