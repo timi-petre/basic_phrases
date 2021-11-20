99bd638e4ceec0d845e448ffbd8b085be4268936
Timi Petre <gaxxis@yahoo.com>
2021-11-20 13:52:46 +0200

    code formated

---
+++ lib/src/choice.dart
@@ -0,0 +1,9 @@
+class Choice {
+  Choice({
+    required this.text,
+    required this.audio,
+  });
+
+  String text;
+  String audio;
+}
---
+++ lib/src/choice_brain.dart
@@ -0,0 +1,46 @@
+import 'choice.dart';
+
+class ChoiceBrain {
+  final List<Choice> choose = <Choice>[
+    Choice(
+      text: 'Salut',
+      audio: 'assets/salut.mp3',
+    ),
+    Choice(
+      text: 'Salut (Engleza)',
+      audio: 'assets/hello.mp3',
+    ),
+    Choice(
+      text: 'Ma numesc',
+      audio: 'assets/ma_numesc.mp3',
+    ),
+    Choice(
+      text: 'Ma numesc (Engleza)',
+      audio: 'assets/my_name_is.mp3',
+    ),
+    Choice(
+      text: 'Cum esti?',
+      audio: 'assets/cum_esti.mp3',
+    ),
+    Choice(
+      text: 'Cum esti? (Engleza)',
+      audio: 'assets/how_are_you.mp3',
+    ),
+    Choice(
+      text: 'Sunt bine',
+      audio: 'assets/sunt_bine.mp3',
+    ),
+    Choice(
+      text: 'Sunt bine (Engleza)',
+      audio: 'assets/im_fine.mp3',
+    ),
+    Choice(
+      text: 'Tu?',
+      audio: 'assets/tu.mp3',
+    ),
+    Choice(
+      text: 'Tu? (Engleza)',
+      audio: 'assets/you.mp3',
+    ),
+  ];
+}
